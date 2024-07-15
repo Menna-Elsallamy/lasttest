28eee449d3373baa6ad9d56d734942c8fb859796
Menna-Elsallamy <mennaelsallamy@gmail.com>
2024-07-16 01:42:51 +0200

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
-
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(cors());
 
 const customers = [
